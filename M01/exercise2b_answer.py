7f4d8368c0881fac96f6c8ca5f05de4d2ce99cd7
naomehmi <125885797+naomehmi@users.noreply.github.com>
2023-09-24 00:45:02 +0700

    Create exercise2b_answer.py

---
+++ M01/exercise2b_answer.py
@@ -0,0 +1,7 @@
+#input
+s = int(input("How many semesters has Mike finished in university?"))
+#output
+if s == 7 or s == 8:
+    print("Mike has graduated university on time")
+else:
+    print("Mike has not graduated university on time")
