d6a50a5f1d1a0e28a03d5b65dbace1b3c33b8dca
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2023-10-12 16:20:38 +0700

    Create fibonacci_seq.py

---
+++ 05 - recursion/fibonacci_seq.py
@@ -0,0 +1,16 @@
+"""
+output the first n numbers of the fibonacci sequence
+"""
+def fibo(n):
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return fibo(n-1) + fibo(n-2)
+
+n = int(input())
+
+for i in range(n):
+    print(fibo(i),end=" ")
+print()
+
+#complexity = O(n)
