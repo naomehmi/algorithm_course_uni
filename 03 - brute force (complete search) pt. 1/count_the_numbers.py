b149f663c9322fa50e7d57e545c841a692917cdb
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2023-10-12 12:40:16 +0700

    Create count_the_numbers.py

---
+++ 03 - brute force (complete search) pt. 1/count_the_numbers.py
@@ -0,0 +1,31 @@
+"""
+in an array of numbers between 0-9 inclusive separated by space, count the amount of 0s, 1s, 2s, ... , 9s in that array and print them each on one line
+
+e.g. 3 8 2 0 4 5 6 9 1 7 3 3 1 2
+
+output:
+1
+2
+3
+3
+1
+1
+1
+1
+1
+1
+
+explanation : in the array, there are 1 number zero, 2 number ones, 3 number twos, 3 number threes, and so on.
+
+"""
+
+arr = list(map(int,input().split()))
+amt = [0 for i in range(10)]
+
+for i in range(len(arr)):
+    amt[arr[i]] += 1
+
+for i in range(10):
+    print(amt[i])
+
+#complexity = O(n)
