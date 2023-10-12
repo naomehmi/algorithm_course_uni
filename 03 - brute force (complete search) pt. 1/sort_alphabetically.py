f9317fcfa3a1b0d2fa7e87ff4a2b25445531e1b8
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2023-10-12 13:03:42 +0700

    Create sort_alphabetically.py

---
+++ 03 - brute force (complete search) pt. 1/sort_alphabetically.py
@@ -0,0 +1,28 @@
+"""
+sort a string alphabetically (excluding whitespaces)
+
+e.g. Hello World
+output = H W d e l l l o o r 
+"""
+
+string = list(input().replace(" ",""))
+nonBrute = string.copy()
+brute = string.copy()
+
+# BRUTE FORCE APPROACH (nested loop) (complexity = O(n * n))
+for i in range(len(brute)):
+    for j in range(len(brute)):
+        if(ord(brute[i]) < ord(brute[j])):
+            brute[i], brute[j] = brute[j], brute[i]
+print("BRUTE FORCE APPROACH :",end=" ")
+for i in range(len(brute)):
+    print(brute[i],end=" ")
+print()
+
+# NON BRUTE FORCE APPROACH (sort with key) (complexity = O(n log n))
+nonBrute.sort(key = lambda x : ord(x))
+
+print("NON BRUTE FORCE APPROACH :",end=" ")
+for i in range(len(nonBrute)):
+    print(nonBrute[i],end=" ")
+print()
