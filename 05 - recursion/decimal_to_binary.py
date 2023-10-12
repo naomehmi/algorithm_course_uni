1ccb2f6cef189e93afa458e10dbd86abbfda0d7b
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2023-10-12 16:36:17 +0700

    Create decimal_to_binary.py

---
+++ 05 - recursion/decimal_to_binary.py
@@ -0,0 +1,13 @@
+"""
+convert a decimal number to binary
+"""
+def convert(n):
+    if n >= 2:
+        convert(n // 2)
+    print(n % 2,end="")
+
+n = int(input("Input a decimal number : "))
+
+convert(n)
+
+#complexity = O(n)
