86479df4e58b43b8c9a54fb13e0011f05df0648b
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2023-10-12 11:40:35 +0700

    Create mike's_graduation.py

---
+++ 01 - introduction to algorithms/mike's_graduation.py
@@ -0,0 +1,12 @@
+"""
+if-else statement to check whether Mike will graduate on time or not
+
+Mike will graduate on time if he has already finished 7 or 8 semesters in college.
+"""
+
+mike = int(input("How many semesters has Mike finished? "))
+
+if mike == 7 or mike == 8:
+    print("Mike will graduate on time.")
+else:
+    print("Mike will not graduate on time")
