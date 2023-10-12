b222dc3bb056dedae22cdf43ef8fb6219a4dadc6
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2023-10-12 13:27:25 +0700

    Create odd_coin_out.py

---
+++ 04 - brute force (complete search) pt. 2/odd_coin_out.py
@@ -0,0 +1,29 @@
+"""
+In an array of 8 alphabetical coins (A-Z), there are 7 identical coins and 1 different coin. Find the position of the different coin. If all 8 coins are the same, output -1
+
+e.g. YXXXXXXX
+output = 1
+
+e.g. BBBABBBB
+output = 4
+
+e.g. BBBBBBBB
+output = -1
+"""
+
+coins = input()
+identifier = coins[0]
+ans = -1
+for i in range(len(coins)):
+    if identifier != coins[i]:
+        if i == 1:
+            if identifier == coins[2]:
+                ans = 2
+            else:
+                ans = 1
+            break
+        else:
+            ans = i + 1
+print(ans)
+
+# complexity = O(n)
