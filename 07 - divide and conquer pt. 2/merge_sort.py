84a3f88be9ae0d25c04c4fccd69ca3b6a0ce33a8
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2023-10-14 10:48:53 +0700

    Create merge_sort.py

---
+++ 07 - divide and conquer pt. 2/merge_sort.py
@@ -0,0 +1,43 @@
+"""
+merge sort algorithm to sort an array of integers
+"""
+def mergeSort(arr,left,middle,right):
+    L = arr[left:middle]
+    R = arr[middle:right]
+    i = 0
+    j = 0
+    k = left
+    while i + left < middle and j + middle < right:
+        if L[i] < R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+    while i + left < middle:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+    while j + middle < right:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+def divide(arr,left,right):
+    if right - left > 1:
+        middle = (left + right) // 2
+        divide(arr,left,middle)
+        divide(arr,middle,right)
+        mergeSort(arr,left,middle,right)
+
+arr = list(map(int,input().split()))
+
+divide(arr,0,len(arr))
+
+print("sorted array :",end=" ")
+for i in arr:
+    print(i,end=" ")
+print()
+
+#complexity : O(log n)
