c350e286a299c3eb31958a83f3045a805d6484b3
Naomi Prisella <125885797+naomehmi@users.noreply.github.com>
2023-10-17 16:44:47 +0700

    Update fibonacci_sequence_memoization.py

--- 08 - dynamic programming pt. 1/fibonacci_sequence_memoization.py
+++ 08 - dynamic programming pt. 1/fibonacci_sequence_memoization.py
@@ -1,9 +1,11 @@
 """
-fibonacci sequence with dynamic programming algorithm
+fibonacci sequence of N numbers, with dynamic programming algorithm
 
 code is from my module
 """
-#recursion
+n = int(input())
+
+#recursion -> O(n)
 def fibonacci(fibo1,n):
     if n == 1:
         return [1]
@@ -14,13 +16,13 @@ def fibonacci(fibo1,n):
         fibo1.append(fibo1[n-3] + fibo1[n-2])
         return fibo1
 
-fibo1 = fibonacci([],11)
+fibo1 = fibonacci([],n)
 
 print("Fibonacci Sequence using memoization with recursion    :",fibo1)
 
-#linear
+#linear -> O(n)
 fibo2 = [1,1]
-for i in range(1,10):
+for i in range(1,n-1):
     fibo2.append(fibo2[i] + fibo2[i-1])
 
 print("Fibonacci Sequence using memoization without recursion :",fibo2)
